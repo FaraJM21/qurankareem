87efd230f2477a05ca83ab379db1ae33f388114a
FaraJM21 <farruxrihsiyev@gmail.com>
2022-10-25 09:14:32 +0500

    a

--- src/routes/Surah.js
+++ src/routes/Surah.js
@@ -1,67 +1,43 @@
 // import axios from "axios";
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import ReactAudioPlayer from "react-audio-player";
 import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-
-
 function Surah() {
   let number = useLocation();
 
-
   const [surahs, setSurahs] = useState(number.state);
-  
+
   const [ayahs, setAyahs] = useState(
     number.state.ayahs.map((item) => item.audio)
   );
 
-
-
   const [isPlay, setIsplay] = useState(false);
   const [num, setNum] = useState(6);
 
-
-
   const playing = (e) => {
-     
-     setTimeout(() => {
-
+    setTimeout(() => {
       if (num + 1 === ayahs.length) {
         document.getElementById("player").pause();
-     
-      }
-      
-      else {
+      } else {
         console.log(`============='kirdi=========`);
         setNum(num + 1);
-       
       }
     }, e.target.duration * 1000);
-    
-   
-       
   };
-
- 
-  
-
-
 
   const play = (inx) => {
     setIsplay(true);
     setNum(inx);
     document.getElementById("player").play();
     document.getElementById("player").autoplay = true;
-  
   };
 
   const pause = () => {
     setIsplay(false);
     document.getElementById("player").pause();
-    document.getElementById("player").autoplay = false; 
-
-   
+    document.getElementById("player").autoplay = false;
   };
 
   return (
@@ -74,9 +50,8 @@ function Surah() {
               controls
               loop
               id="player"
-              onPlay = {playing}
+              onPlay={playing}
               // onPause = {stop}
-           
             />
 
             {surahs.ayahs.map((item, index) => {
@@ -117,26 +92,3 @@ function Surah() {
 }
 
 export default Surah;
-
-// surahs.ayahs.map((item, index) => {
-//  return (
-//    <>
-//      <div className={`cards  ${index}`} key={item.number}>
-//        <div className="inner">
-//          <div className="number">
-//            <p>{item.number}</p>
-//            <span>:</span>
-//            <p>{item.numberInSurah}</p>
-//          </div>
-//          <div className="player">
-//            <i> {play ? <BsPauseFill /> : <BsFillPlayFill />}</i>
-//          </div>
-//          <span>{item.sajda.toString()}</span>
-//        </div
-//        <div className="title">
-//          <h3>{item.text}</h3>
-//        </div>
-//      </div>
-//    </>
-//  );
-//  }
